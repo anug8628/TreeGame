6764097d88c8f6ee46b87ca74e0e53c158f2df48
Anushka <Anushka@Ravis-Air.lan>
2018-11-10 21:49:51 -0800

    black screen with red apple

---
+++ app/src/main/java/com/example/anushka/codeday2018/GameView.java
@@ -0,0 +1,11 @@
+package com.example.anushka.codeday2018;
+
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by Anushka on 11/10/18.
+ */
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+}
